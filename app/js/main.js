742a4f9423fd7cf8cec83c794c4564146dc82e2c
Dima121111 <157640077+Dima121111@users.noreply.github.com>
2026-01-09 23:26:51 +0100

    Header responsiveness

--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
     const langSwitcher = document.querySelector('[data-lang-switcher]');
     const langList = langSwitcher.nextElementSibling;
-
-    // Відкриття / закриття по кнопці
     langSwitcher.addEventListener('click', function (e) {
         e.stopPropagation();
 
         const isActive = langList.classList.toggle('active');
         this.classList.toggle('active', isActive);
     });
-
-    // Клік по екрану — закриваємо список
     document.addEventListener('click', function (e) {
         if (!e.target.closest('.switcher-lang')) {
             langList.classList.remove('active');
             langSwitcher.classList.remove('active');
         }
+    });
+
+    const menuBtnRef = document.querySelector("[data-menu-button]");
+    const mobileMenuRef = document.querySelector("[data-menu]");
+    menuBtnRef.addEventListener("click", () => {
+        const expanded =
+            menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+        menuBtnRef.classList.toggle("is-open");
+        menuBtnRef.setAttribute("aria-expanded", !expanded);
+        mobileMenuRef.classList.toggle("is-open");
     });
 });
